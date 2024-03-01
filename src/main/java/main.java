7dbfbd9cfb7e2b0e43b6f1e8bf5ceede81817878
Igorpereirag <igorpereira1501@gmail.com>
2024-03-01 11:33:05 -0300

    fix: class main

---
+++ src/main/java/main.java
@@ -0,0 +1,15 @@
+import model.Tradutor;
+
+public class main {
+    public static void main(String[] args) {    
+        Tradutor tradutor = new Tradutor();
+         String palavraTraduzida = tradutor.traduzir("cat"); 
+         System.out.println(palavraTraduzida);
+
+
+
+
+  
+
+    }
+}
